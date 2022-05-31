1622ff47a7d4200acc784d9b198f720cd68ba0a4
Gabriel-Hiago95 <101152093+Gabriel-Hiago95@users.noreply.github.com>
2022-05-31 20:35:45 -0300

    Delete semana-05/T1 directory

--- semana-05/T1/Sem-05-T1-Q01.py
+++
@@ -1,8 +0,0 @@
-def main():
-    def cont_carac(nome):
-        cont = len(nome)
-        return f'{cont}'
-    nom = input().strip()
-    print(cont_carac(nom))
-if __name__ == '__main__':
-    main()
--- semana-05/T1/Sem-05-T1-Q02.py
+++
@@ -1,8 +0,0 @@
-def main():
-    def codigo_carac(digito):
-        cod = ord(digito)
-        return f'{cod}'
-    caractere = input().strip()
-    print(codigo_carac(caractere))
-if __name__ == '__main__':
-    main()
